05915a50c9540eb43b99bf2815372410494ea19a
A-U-Said <adam.utton.said@gmail.com>
2022-02-15 13:35:13 +0000

    Simulate GET

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useEffect, useState } from "react";
 import Tree from "./components/Tree";
 import TreeData from "./components/TreeData";
+import beehiveLogo from './beehive.svg';
 
 const App = (): JSX.Element => {
+
+  const [treeMembers, setTreeMembers] = useState<dragList[] | null>(null);
+
+  const fetchMembers = (): Promise<dragList[]> => {
+    const fetchedTreeMembers = TreeData;
+    return Promise.resolve(fetchedTreeMembers);
+  };
+
+  useEffect(() => {
+    setTimeout(() => fetchMembers().then(members => { setTreeMembers(members) }), 1000); //simulate get
+  }, [])
+
+  const loading: JSX.Element = <img className="loading-logo" src={beehiveLogo}  alt="Beehive honeycomb logo"></img>;
 
   const onChange = (message: string): void => {
     console.log(message);
@@ -10,7 +25,8 @@ const App = (): JSX.Element => {
   return (
     <div className="App">
       <h1>Dragover for 0.75s to insert as child or drop immediately to add as sibling</h1>
-      <Tree members={TreeData} onChange={onChange}/>
+      { !treeMembers && loading }
+      { treeMembers && <Tree members={treeMembers} onChange={onChange}/> }
     </div>
   );
 };
--- src/components/TreeData.ts
+++ src/components/TreeData.ts
@@ -1,4 +1,4 @@
-export default [
+const treeData = [
     {
         name: 'Subjects',
         value: "",
@@ -53,3 +53,5 @@ export default [
         ]
     }
 ];
+
+export default treeData;
